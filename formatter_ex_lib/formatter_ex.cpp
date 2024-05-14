8369124230f79f3be0c5104e39e56e11d64c1f18
Titanoboba <nik20052006@mail.ru>
2024-05-14 12:52:59 +0300

    Deleted everything

--- formatter_ex_lib/formatter_ex.cpp
+++
@@ -1,8 +0,0 @@
-#include "formatter_ex.h"
-
-#include "../formatter_lib/formatter.h"
-
-std::ostream& formatter(std::ostream& out, const std::string& message)
-{
-    return out << formatter(message);
-}
